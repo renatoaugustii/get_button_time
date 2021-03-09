8f4907b233eaffbd0e5c6561a2c2b6c695be85d6
Renato Augusto <40027879+renatoaugustii@users.noreply.github.com>
2021-03-09 15:01:16 -0300

    Creat a new method to get clicked times.
    Use GET_CLICKED_TIMES to get the number of times the button was pressed. After the TIMER_RESET time, the value of the variable returns to zero.

--- Get_Button_Time/Get_Button_Time.cpp
+++ Get_Button_Time/Get_Button_Time.cpp
@@ -3,7 +3,7 @@
    
    Author: Eng. Renato Augusto
       
-   Github: https://github.com/renatoaugustii/????
+   Github: https://github.com/renatoaugustii/get_button_time
    Atribuição-NãoComercial -  Sujeito a punições legais
    
    **  VERSION CONTROLLER **
@@ -31,11 +31,14 @@ long get_b::get_time()
      {
           if (_btn_flag == false)
           { _tempo_inicial = millis();_btn_flag = true;_button_pressed = true;}
+	  if (_last_state == false){cont = cont++;_last_state = true;}  
 		  return 0;	
       }
 
       else
       { 
+	if (_last_state == true){_end_click_time = millis();}      
+	_last_state = false;      
         if (_button_pressed == true)
            {
                  _btn_flag = false;
@@ -43,6 +46,13 @@ long get_b::get_time()
 				 Button_time = (millis()-_tempo_inicial);
                  return Button_time;
              }
+	         
 		 return Button_time;	 
         }	
   }
+
+int get_b::get_clicked_times(int timer_reset);
+{
+	if ((millis()-_end_click_time)<=timer_reset){cont = 0;}
+	return cont;
+}
